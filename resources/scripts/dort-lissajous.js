f114becee2b1bd1c39293db07ff80bae25b46bb5
paul casey <paulcaseys@gmail.com>
2023-04-24 14:21:59 +1000

    adding retina / hidpi support

--- resources/scripts/dort-lissajous.js
+++ resources/scripts/dort-lissajous.js
@@ -20,18 +20,40 @@ function initLissajousCurve(obj, canvas) {
     if (!canvas || !canvas.getContext) { return false; }
     var ctx = canvas.getContext('2d');
 
+
     obj.increment = obj.increment || 0;
     obj.xStep = obj.xStep || 2;
     obj.yStep = obj.yStep || 5;
     obj.speed = obj.speed || 0.008;
     obj.n = obj.n || 50;
-    obj.base = obj.base  || { x: 100, y: 100, xSize: 2, ySize: 2};
-    obj.radius = obj.radius || 70;
+    obj.hidpi = false;
+    obj.canvasWidth = obj.canvasWidth || 400;
+    obj.canvasHeight = obj.canvasHeight || 400;
+    obj.xMid = obj.xMid || obj.canvasWidth / 2;
+    obj.yMid = obj.yMid || obj.canvasHeight / 2;
+    obj.radius = obj.radius || 190;
     obj.lineWidth = obj.lineWidth || 4;
     obj.intervalAmount = obj.intervalAmount || 50;
     obj.strokeStyle01 = obj.strokeStyle01 || 'rgb(255, 0, 0)';
     obj.strokeStyle02 = obj.strokeStyle02 || 'rgb(0, 255, 0)';
 
+    // retina is set via hidpi
+    if (obj.hidpi === false) {
+        canvas.style.width = obj.canvasWidth + "px";
+        canvas.style.height = obj.canvasHeight + "px";
+        canvas.width = obj.canvasWidth;
+        canvas.height = obj.canvasHeight;
+    } else {
+        canvas.width = obj.canvasWidth*2;
+        canvas.height = obj.canvasHeight*2;
+        obj.xCentre = obj.xCentre/2;
+        obj.yCentre = obj.yCentre/2;
+        canvas.style.width = "400px";
+        canvas.style.height = "400px";
+        ctx.scale(2,2);
+    }
+
+    // sets interval (timer for FPS)
     setInterval(drawLC, obj.intervalAmount, obj, canvas, ctx);
 
 }
@@ -43,32 +65,30 @@ function drawLC(obj, canvas, ctx) {
     var yStep = obj.yStep + 0.002;
     var n = obj.n;
     var scale = 1;
-    var base = obj.base
 
-    var radius = obj.radius;
     for (j = 0; j <= 2 * Math.PI; j += Math.PI / n / xStep) {
-        
+
         var jOffset = j;
 
 
         ctx.beginPath();
-        ctx.moveTo((base.x + x)*base.xSize, (base.y - y)*base.ySize);
+        ctx.moveTo((obj.xMid + x), (obj.yMid - y));
 
 
-        if(j < Math.PI){
+        if (j < Math.PI) {
             ctx.strokeStyle = obj.strokeStyle01;
         } else {
             ctx.strokeStyle = obj.strokeStyle02;
         }
 
-        var x = (radius * Math.sin((scale)*((xStep) * (jOffset)+obj.increment)));
-        var y = (radius * Math.cos((scale)*((yStep) * (jOffset))));
-        
-        ctx.lineTo((base.x + x)*base.xSize, (base.y - y)*base.ySize);
+        var x = (obj.radius * Math.sin((scale) * ((xStep) * (jOffset) + obj.increment)));
+        var y = (obj.radius * Math.cos((scale) * ((yStep) * (jOffset))));
+
+        ctx.lineTo((obj.xMid + x), (obj.yMid - y));
         ctx.lineWidth = obj.lineWidth;
         ctx.stroke();
         ctx.closePath();
-        
+
     }
     obj.increment = obj.increment + obj.speed;
 }
